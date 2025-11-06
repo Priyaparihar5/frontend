b940dc7e7786c5b2b6b1d7f890034c8df62d4151
Priya Parihar <priyaparihar1605@gmail.com>
2025-11-06 10:44:29 +0530

    practice

---
+++ HTML/ASSIGNMENT/experiment/script.js
@@ -0,0 +1,15 @@
+
+//task 1
+// const username = prompt("hellow welcome to this stupid webpage,what is your name");
+//alert("hello"+ username + " , welcome to js promming");
+
+
+//task 2
+//const age = prompt("Enter your name:");
+//alert ((age>=18)?"you are eligible to vote" : "you are not eligible to vote");
+
+
+const age = prompt("enter your age:");
+ age >= 18
+   ? document.write("<h1> you are an adult.")
+   : document.write("you are a minor");
